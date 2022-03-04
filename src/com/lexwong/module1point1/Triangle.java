677cdfba524fc4c88b6f0044b81427c17b9d5a92
levxxvi <99051920+levxxvi@users.noreply.github.com>
2022-03-04 14:38:03 -0800

    committing just in case. module.1.1 assignment in progress

--- src/com/lexwong/module1point1/Triangle.java
+++ src/com/lexwong/module1point1/Triangle.java
@@ -1,4 +1,25 @@
 package com.lexwong.module1point1;
 
-public class Triangle {
+public class Triangle extends TwoDShape{
+    double side1;
+    double side2;
+    //base is side 3
+    double side3;
+    public Triangle(double width, double height) {
+        super(width, height);
+    }
+    public Triangle(double side1, double side2,double side3){}
+
+    private double heronsHeight(){
+        getArea();
+        double h = getArea()*2*side3;
+        return h;
+    }
+
+    public double getArea(){
+        double s;
+        s = (side1 + side2 + side3)/2;
+        double area = Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
+        return area;
+    }
 }
--- src/com/lexwong/module1point1/TwoDShape.java
+++ src/com/lexwong/module1point1/TwoDShape.java
@@ -1,4 +1,12 @@
 package com.lexwong.module1point1;
 
 public class TwoDShape {
+    double width;
+    double height;
+    public TwoDShape(double width, double height){}
+    public TwoDShape(){}
+    public double getArea(){
+        return 0;
+    }
+    public void setHeight(double height){}
 }
